27b09a8bb934000f3fa1532c1a390c035f96f2c5
Jaynesh1609 <54092967+Jaynesh1609@users.noreply.github.com>
2022-01-23 23:42:54 +0530

    removing path..

--- aws_setup/variable.tf
+++ aws_setup/variable.tf
@@ -4,6 +4,11 @@ variable "name" {
 variable "region" { 
   default = "ap-south-1" 
 }
+
+//as i have used  s3. for backend so that's why i'm comminting out 
+
+/*
 variable "path" { 
   default = "../vault-setup/terraform.tfstate" 
 }
+*/
