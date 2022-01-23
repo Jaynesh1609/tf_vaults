d101fabb45aa6c19f7c2ee61986f6e9990b411ef
Jaynesh1609 <54092967+Jaynesh1609@users.noreply.github.com>
2022-01-23 19:51:04 +0530

    Add files via upload

---
+++ aws_setup/ec2.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.23.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "2.17.0"
+    }
+  }
+}
+
+
+variable "name" { default = "aws-with-vault" }
+variable "region" { default = "ap-south-1" }
+variable "path" { default = "../vault-setup/terraform.tfstate" }
+#variable "ttl" { default = "1" }
+
+
+// this is our tfstate config about ec2.tf
+
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+
+// this is about vault tfstate file
+
+
+data "terraform_remote_state" "vaultest" {
+  backend = "local"
+  config = {
+    path = var.path
+  }
+}
+
+
+//this will contact with you vault an trying to give you key in accordance with
+
+
+data "vault_aws_access_credentials" "vaultcred" {
+  backend = data.terraform_remote_state.vaultest.outputs.backend
+  role    = data.terraform_remote_state.vaultest.outputs.role
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = data.vault_aws_access_credentials.vaultcred.access_key
+  secret_key = data.vault_aws_access_credentials.vaultcred.secret_key
+}
+
+
+# Create AWS EC2 Instance
+resource "aws_instance" "test" {
+  ami           = "ami-0af25d0df86db00c1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.name
+  }
+}
